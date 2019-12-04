44c9875c809d72e2671cc88958b80195f8575199
alanamfanelli <56361850+alanamfanelli@users.noreply.github.com>
2019-12-04 15:25:02 -0700

    add http requests

--- index.js
+++ index.js
@@ -19,14 +19,6 @@ let topMovies = [ {
 }
 ]
 
-
-app.get('/movies', function(req, res) {
-  res.json(topMovies)
-});
-app.get('/', function(req'res) {
-  res.send('Welcome to myFlix!')
-});
-
 app.use(express.static('public'));
 app.use(morgan("common"));
 
@@ -36,4 +28,56 @@ app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500).send("Something broke!");
   next();
+});
+
+// Gets list of all movies
+app.get("/movies", function(req, res) {
+  res.send("Successful GET request returning data about all movies.");
+});
+
+// Gets the data about a single movie by title
+app.get("/movies/:title", (req, res) => {
+  res.send("Successful GET request returning data about a single movie.");
+});
+
+// Gets the data about a movie genre by name
+app.get("/genre/:name", (req, res) => {
+  res.send("Successful GET request returning data about a movie genre.");
+});
+
+// Gets the data about a director by name
+app.get("/director/:name", (req, res) => {
+  res.send("Successful GET request returning data about a director.");
+});
+
+// Adds data for a new user
+app.post("/users", (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.username) {
+    const message = "Missing username in request body";
+    res.status(400).send(message);
+  } else {
+    res.send("User successfully added.");
+  }
+});
+
+// Update the a user's information
+app.put("/users/:username/:password/:email/:dateofbirth", (req, res) => {
+  res.send("User information updated.");
+});
+
+// Adds movie to favorites for a user
+app.post("/favorites/:username/:title", (req, res) => {
+  res.send("add favorite movie by user.");
+});
+
+// Deletes a movie from a user's favorites list by username
+app.delete("/favorites/:username/:title", (req, res) => {
+  res.send("Movie successfully deleted from favorites.");
+});
+
+// Deletes a user from the user registry
+app.delete("/users/:username", (req, res) => {
+  res.send("User successfully deleted from registry.");
 });
