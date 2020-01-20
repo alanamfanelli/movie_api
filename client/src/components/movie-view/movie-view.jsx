53459362a720797cc0b64121bc7fa637457ad886
alanamfanelli <56361850+alanamfanelli@users.noreply.github.com>
2020-01-20 13:01:21 -0700

    add back button

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -15,31 +15,30 @@ export class MovieView extends React.Component {
 
         return (
             <div className="movie-view">
-                <div
-                    className="movie-go-back"
-                    onClick={() => onClick()}
-                >Go back to the list of movies</div>
-                <img className="movie-poster" src={movie.ImagePath} />
-                <div className="movie-title">
-                    <span className="label">Title: </span>
-                    <span className="value">{movie.Title}</span>
-                </div>
-                <div className="movie-description">
-                    <span className="label">Description: </span>
-                    <span className="value">{movie.Description}</span>
-                </div>
+                <form>
+                    <input type="button" value="Go back!" onclick="history.back()">
+                </form>
+                    <img className="movie-poster" src={movie.ImagePath} />
+                    <div className="movie-title">
+                        <span className="label">Title: </span>
+                        <span className="value">{movie.Title}</span>
+                    </div>
+                    <div className="movie-description">
+                        <span className="label">Description: </span>
+                        <span className="value">{movie.Description}</span>
+                    </div>
 
-                <div className="movie-genre">
-                    <span className="label">Genre: </span>
-                    <span className="value">{movie.Genre.Name}</span>
-                </div>
-                <div className="movie-director">
-                    <span className="label">Director: </span>
-                    <span className="value">{movie.Director.Name}</span>
-                </div>
+                    <div className="movie-genre">
+                        <span className="label">Genre: </span>
+                        <span className="value">{movie.Genre.Name}</span>
+                    </div>
+                    <div className="movie-director">
+                        <span className="label">Director: </span>
+                        <span className="value">{movie.Director.Name}</span>
+                    </div>
             </div>
 
 
-        );
-    }
+                );
+            }
 }
