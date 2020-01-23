8caaf78f4fad29a6a2da96e48f9727c24e75b2d7
alanamfanelli <56361850+alanamfanelli@users.noreply.github.com>
2020-01-23 14:30:07 -0700

    parcel

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -35628,7 +35628,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50964" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61443" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
