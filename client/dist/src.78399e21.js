bdb37082925ee875951cf02f62fcf8445eae562f
alanamfanelli <56361850+alanamfanelli@users.noreply.github.com>
2020-02-10 14:52:15 -0700

    added container fluid to DOM

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -55537,7 +55537,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59836" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65484" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
